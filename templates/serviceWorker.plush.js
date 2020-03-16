2d39d86c1ef44bcea0622cf86c2a98e14a77e6b1
Jochen <rothjochen@gmail.com>
2020-03-17 00:24:25 +0100

    optimize service worker

--- templates/serviceWorker.plush.js
+++ templates/serviceWorker.plush.js
@@ -20,70 +20,77 @@ let cacheFiles = [
     '/assets/icon/iconfont.woff',
     '/assets/font/Oswald-VariableFont_wght.ttf',
     '/api/groups/',
-    '/api/devices/',
-    '/api/pins/',
 ]
 
-self.addEventListener('install', function(e) {
-    e.waitUntil(
-        caches.open(cacheName).then(function(cache) {
-            logger('Caching cacheFiles');
-            return cache.addAll(cacheFiles);
-        })
-    );
+let ignoreMethod = ['POST', 'DELETE', 'PUT'];
+// if(!ignoreMethod.includes(evt.request.method)) {
+//
+// }
+
+
+let CACHE = 'cache-update-and-refresh';
+
+
+self.addEventListener('install', function(evt) {
+    console.log('The service worker is being installed.');
+    evt.waitUntil(caches.open(CACHE).then(function (cache) {
+        cache.addAll([
+            '/',
+            '/groups/',
+            '/devices/',
+            '/pins/',
+            '/assets/images/logo.svg',
+            '/assets/images/manifest-logo.svg',
+            '/assets/application.css',
+            '/assets/application.js',
+            '/assets/icon/iconfont.eot',
+            '/assets/icon/iconfont.svg',
+            '/assets/icon/iconfont.ttf',
+            '/assets/icon/iconfont.woff',
+            '/assets/font/Oswald-VariableFont_wght.ttf',
+        ]);
+    }));
 });
 
-self.addEventListener('activate', function(e) {
-    logger('Activated');
+self.addEventListener('fetch', function(evt) {
+    console.log('The service worker is serving the asset.');
 
-    e.waitUntil(
-        caches.keys().then(function(cacheNames) {
-            return Promise.all(cacheNames.map(function(thisCacheName) {
-                if (thisCacheName !== cacheName) {
-                    logger('Removing Cached Files from Cache - ' + thisCacheName);
-                    return caches.delete(thisCacheName);
-                }
-            }));
-        })
-    );
-
-    return self.clients.claim();
+    if(!ignoreMethod.includes(evt.request.method)) {
+        evt.waitUntil(
+            update(evt.request)
+                .then(refresh)
+        );
+        evt.respondWith(fromCache(evt.request));
+    }
 });
 
-self.addEventListener('fetch', function(e) {
-    logger('Fetch', e.request.url);
-    e.respondWith(
-        caches.match(e.request)
-            .then(function(response) {
-                if ( response ) {
-                    logger('Found in cache ' + e.request.url);
+function fromCache(request) {
+    return caches.open(CACHE).then(function (cache) {
+        return cache.match(request);
+    });
+}
+
+function update(request) {
+    return caches.open(CACHE).then(function (cache) {
+        return fetch(request).then(function (response) {
+            if(!ignoreMethod.includes(request.method)) {
+                return cache.put(request, response.clone()).then(function () {
                     return response;
-                } else {
-                    logger('Not found in cache ' + e.request.url);
-                }
+                });
+            }
+        });
+    });
+}
 
-                let requestClone = e.request.clone();
-                return fetch(requestClone)
-                    .then(function(response) {
-
-                        if ( !response ) {
-                            logger('No response from fetch')
-                            return response;
-                        }
-
-                        let responseClone = response.clone();
-
-                        caches.open(cacheName).then(function(cache) {
-                            cache.put(e.request, responseClone);
-                            logger('New Data Cached ' + e.request.url);
-                            return response;
-
-                        });
-
-                    })
-                    .catch(function(err) {
-                        logger('Error Fetching & Caching New Data', err);
-                    });
-            })
-    );
-});
+function refresh(response) {
+    return self.clients.matchAll().then(function (clients) {
+        clients.forEach(function (client) {
+            let message = {
+                type: 'refresh',
+                url: response.url,
+                eTag: response.headers.get('ETag')
+            };
+            client.postMessage(JSON.stringify(message));
+        });
+    });
+}
